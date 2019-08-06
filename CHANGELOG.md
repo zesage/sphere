e44eb204fc415f3329d0ef89165520442b863aba
Mark Hu <zesage@gmail.com>
2019-08-06 16:55:14 +0800

    update example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [0.0.3] - 8/4/2019
+
+- optimized buildsphere
+- update example
+
+
 ## [0.0.2] - 8/4/2019
 
 - add drag and zoom gesture
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,8 +24,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double latitude = 0;
-  double longitude = 0;
+  String surface = '2k_earth_daymap.jpg';
+  final surfaces = [
+    '2k_sun.jpg',
+    '2k_mercury.jpg',
+    '2k_venus_surface.jpg',
+    '2k_earth_daymap.jpg',
+    '2k_moon.jpg',
+    '2k_mars.jpg',
+    '2k_jupiter.jpg',
+    '2k_saturn.jpg',
+    '2k_uranus.jpg',
+    '2k_neptune.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +44,49 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Sphere(
-          surface: 'assets/2k_earth_daymap.jpg',
-          radius: 180,
-          latitude: latitude,
-          longitude: longitude,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/2k_stars.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Sphere(
+                  key: ValueKey(surface),
+                  surface: 'assets/' + surface,
+                  radius: 160,
+                  latitude: 15,
+                  longitude: 0,
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                itemExtent: 200,
+                children: surfaces.map((item) {
+                  return ListTile(
+                    title: Column(
+                      children: <Widget>[
+                        Text(item),
+                        Image.asset('assets/' + item),
+                      ],
+                    ),
+                    onTap: () {
+                      setState(() {
+                        surface = item;
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
         ),
       ),
     );
